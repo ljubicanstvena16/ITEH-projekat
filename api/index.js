a2c17caede34b8080bef4cca7a306d9c6bb4de97
ljubicanstvena16 <joksiiib@gmail.com>
2023-08-22 17:59:16 +0200

    funkcionalnost prijavljivanja i registrovanja

--- api/index.js
+++ api/index.js
@@ -1,8 +1,87 @@
 const express = require('express');
+const cors = require('cors');
+
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs');
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
+
+const User = require('./models/User.js');
+
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin:'http://localhost:5173',
+    setHeader:'*'
+}));
+
+mongoose.connect(process.env.MONGO_URL);
 
 app.get('/test', (req,res) => {
     res.json('test ok');
+});
+
+//Registracija
+app.post('/api/register', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {name,email,password} = req.body;
+
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+
+});
+
+//Prijavljivanje
+app.post('/api/login', async (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('Password nije u redu! Pokusaj ponovo.');
+    }
+  } else {
+    res.json('Korisnik nije pronadjen! Registruj se ukoliko nemas nalog.');
+  }
+});
+
+//Vracanje informacije o prijavljenom profilu
+app.get('/api/profile', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
 });
 
 app.listen(4000);
