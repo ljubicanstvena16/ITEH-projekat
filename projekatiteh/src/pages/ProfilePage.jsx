046ae31b09fc2b8918f6cd87bd20a95c42fa8a43
majabrankovic <brankovicm30@gmail.com>
2023-08-24 15:41:45 +0200

    Izgled stranica za prikaz mesta za smestaj i dodavanje novog

--- projekatiteh/src/pages/ProfilePage.jsx
+++ projekatiteh/src/pages/ProfilePage.jsx
@@ -40,6 +40,7 @@ export default function ProfilePage() {
                     </button>
                 </div>
             )}
+            {subpage === "places" && <PlacesPage />}
         </div>
     );
 }
