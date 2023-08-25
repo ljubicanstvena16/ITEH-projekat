f3480524bd3c9ec7eb102d87d063fc1c6df5ce3a
majabrankovic <brankovicm30@gmail.com>
2023-08-25 14:31:16 +0200

    funkcionalnost uploada fotografija preko linka

--- api/index.js
+++ api/index.js
@@ -11,11 +11,14 @@ const User = require('./models/User.js');
 
 const cookieParser = require('cookie-parser');
 
+const imageDownloader = require('image-downloader');
+
 require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use('/uploads', express.static(__dirname+'/uploads'));
+app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin:'http://localhost:5173',
@@ -86,6 +89,17 @@ app.get('/api/profile', (req,res) => {
 
 app.post('/api/logout', (req,res) => {
   res.cookie('token', '').json(true);
+});
+
+app.post('/api/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: '/tmp/' +newName,
+  });
+  const url = await uploadToS3('/tmp/' +newName, newName, mime.lookup('/tmp/' +newName));
+  res.json(url);
 });
 
 app.listen(4000);
